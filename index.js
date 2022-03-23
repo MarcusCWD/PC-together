b8076e09e61a85fd82fdce2a142b95dc0584f33e
Marcus <marcus.chong@trentglobal.edu.sg>
2022-03-23 07:06:19 +0000

    edit build page route, compare email

--- index.js
+++ index.js
@@ -43,11 +43,9 @@ async function main(){
              "all-build":mainList
         })
     })
-    
     //====================BUILD: FILTER (BRAND) MAIN LOAD==========================//
     //READ
-    app.get('/build/:brand_name', async function(req,res){
-
+    app.get('/:brand_name/build', async function(req,res){
         const db = MongoUtil.getDB();
         let mainList = await db.collection(BUILD_COLLECTION).find({
             'cpu_brand': {
@@ -64,10 +62,9 @@ async function main(){
         })
     })
     // axios.get("http://ldkndkfn"+ "/main/brand/ " +this.state.searchBrand)
-
     //====================BUILD: FILTER (PRICE) MAIN LOAD==========================//
     //READ
-    app.get('/build/:price1/:price2', async function(req,res){        
+    app.get('/:price1/:price2/build', async function(req,res){        
         const db = MongoUtil.getDB();
         let mainList = await db.collection(BUILD_COLLECTION).find({
             'price' : {
@@ -111,7 +108,6 @@ async function main(){
         let ramItem = await db.collection(RAM_COLLECTION).find({
             '_id': mainList[0].parts.ram_id
         }).toArray();
-    
 
         res.send({
             "build" : mainList,
@@ -124,23 +120,53 @@ async function main(){
     })
     //====================BUILD: INDIVIDUAL PAGE COMMENTS SEND==========================//
     //CREATE
-    app.post('/:id/individualbuild', async function(req,res){
+    app.post('/:id/individualbuild/comment', async function(req,res){
         try {
-        
-        let name = req.body.name
-        let comment = req.body.comment
-        let build_id = ObjectId(req.params.id)
-        let email = req.body.email
-        let datetime = new Date().toLocaleString(); //3/22/2022, 2:34:36 PM
+            let name = req.body.name
+            let comment = req.body.comment
+            let build_id = ObjectId(req.params.id)
+            let email = req.body.email
+            let datetime = new Date().toLocaleString(); //3/22/2022, 2:34:36 PM
 
-        const db = MongoUtil.getDB();
-        await db.collection(COMMENTS_COLLECTION).insertOne({
-            name,
-            comment,
-            build_id,
-            email,
-            datetime
-        });
+            const db = MongoUtil.getDB();
+            await db.collection(COMMENTS_COLLECTION).insertOne({
+                name,
+                comment,
+                build_id,
+                email,
+                datetime
+            });
+            res.status(200);
+            res.json({
+                'message':'The record has been added successfully'
+            })
+        } catch (e){
+            res.status(500);
+            res.json({
+                'message':"Internal server error. Please contact administrator"
+            })
+            console.log(e);
+        }
+    })
+    //====================BUILD: INDIVIDUAL PAGE UPVOTE EDIT==========================//
+    //UDATE (abit of details)
+    app.patch('/:id/individualbuild/voteup', async function(req,res){
+        try {
+            const db = MongoUtil.getDB();
+            let upvote = await db.collection(BUILD_COLLECTION).find({
+                '_id': ObjectId(req.params.id)
+            }).project({
+                'votes': 1
+            }).toArray();
+            
+            let updateVotes = upvote[0].votes + 1
+            await db.collection(BUILD_COLLECTION).updateOne({
+                '_id': ObjectId(req.params.id)
+            },{
+            '$set':{
+                'votes': updateVotes,
+            }
+        })
         res.status(200);
         res.json({
             'message':'The record has been added successfully'
@@ -153,6 +179,136 @@ async function main(){
             console.log(e);
         }
     })
+    //====================BUILD: INDIVIDUAL PAGE CHANGE==========================//
+    //READ
+    app.get('/:id/:email/individualbuild', async function(req,res){
+        
+            const db = MongoUtil.getDB();
+            let mainList = await db.collection(BUILD_COLLECTION).find({
+                '_id' : ObjectId(req.params.id)
+            }).project({
+                'email' : 1
+            }).toArray();
+
+            if(mainList[0].email === req.params.email){
+                res.send({
+                    "message": true,
+                })
+            }
+            else{
+                res.send({
+                    "message": false
+                })
+            }
+    })
+    //====================BUILD: NEW LISTING PARTS LOAD==========================//
+    //READ
+    app.get('/newbuild', async function(req,res){
+        const db = MongoUtil.getDB();
+        //get cpu details
+        let cpuItem = await db.collection(CPU_COLLECTION).find({}).project({
+            '_id': 1,
+            'product_name': 1,
+            'image': 1,
+        }).toArray();
+        //get gpu details
+        let gpuItem = await db.collection(GPU_COLLECTION).find({}).project({
+            '_id': 1,
+            'product_name': 1,
+            'image': 1,
+        }).toArray();
+        //get mobo details
+        let moboItem = await db.collection(MOBO_COLLECTION).find({}).project({
+            '_id': 1,
+            'product_name': 1,
+            'image': 1,
+        }).toArray();
+        //get mobo details
+        let ramItem = await db.collection(RAM_COLLECTION).find({}).project({
+            '_id': 1,
+            'name': 1,
+            'image': 1,
+        }).toArray();
+
+        res.send({
+             "cpu" : cpuItem,
+             "gpu" : gpuItem,
+             "mobo" : moboItem,
+             "ram" : ramItem,
+        })
+    })
+    //====================BUILD: NEW LISTING PART AND FORM SEND==========================//
+    //CREATE
+    app.post('/newbuild', async function(req,res){
+        try {
+            const db = MongoUtil.getDB();
+            let cpuList = await db.collection(CPU_COLLECTION).find({
+                '_id': ObjectId(req.body.cpu)
+            }).project({
+                'brand' : 1,
+                'price' : 1
+            }).toArray();
+
+            let gpuList = await db.collection(GPU_COLLECTION).find({
+                '_id': ObjectId(req.body.gpu)
+            }).project({
+                'price' : 1
+            }).toArray();
+
+            let moboList = await db.collection(MOBO_COLLECTION).find({
+                '_id': ObjectId(req.body.mobo)
+            }).project({
+                'price' : 1
+            }).toArray();
+
+            let ramList = await db.collection(RAM_COLLECTION).find({
+                '_id': ObjectId(req.body.ram)
+            }).project({
+                'price' : 1
+            }).toArray();
+
+            let name = req.body.name
+            let build_ease = req.body.build_ease
+            let image = req.body.image
+            let price = parseFloat(cpuList[0].price) + parseFloat(gpuList[0].price) + parseFloat(moboList[0].price) + parseFloat(ramList[0].price)
+            let description = req.body.description
+            let datetime = new Date().toLocaleString() //3/22/2022, 2:34:36 PM
+            let votes = 0
+            let cpu_brand = cpuList[0].brand
+            let email = req.body.email
+
+            //client side has id of individual part. pass over to backend fron dropdown list
+            let parts = {
+                'cpu_id': ObjectId(req.body.cpu),
+                'gpu_id': ObjectId(req.body.gpu),
+                'mobo_id': ObjectId(req.body.mobo),
+                'ram_id': ObjectId(req.body.ram)
+            }
+            await db.collection(BUILD_COLLECTION).insertOne({
+                name,
+                build_ease,
+                image,
+                price,
+                description,
+                datetime,
+                votes,
+                cpu_brand,
+                parts,
+                email
+            });
+            res.status(200);
+            res.json({
+                'message':'The record has been added successfully'
+            })
+        } catch (e){
+            res.status(500);
+            res.json({
+                'message':"Internal server error. Please contact administrator"
+            })
+            console.log(e);
+        }
+    })
+
     //====================PARTS: CPU==========================//
     //READ
     app.get('/cpu', async function(req,res){
