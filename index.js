995623417b1b5d0a2022f87a5714c1d3be891fed
Marcus <marcus.chong@trentglobal.edu.sg>
2022-03-28 10:39:59 +0000

    added edit route for individual listing

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 const MongoUtil = require('./MongoUtil');
 
-
 // SETUP
 const app = express();
 
@@ -39,47 +38,33 @@ async function main(){
             'image': 1,
             'vote' : 1,
         }).toArray();
-        res.json({
+        res.send({
              "all-build":mainList
         })
     })
-    //====================BUILD: FILTER (BRAND) MAIN LOAD==========================//
+    //====================BUILD: FILTER (BRAND, PRICE) MAIN LOAD==========================//
     //READ
-    app.get('/:brand_name/build', async function(req,res){
+    app.get('/build/search', async function(req,res){
+        // console.log(req.query.priceUpper, req.query.priceLower)
         const db = MongoUtil.getDB();
         let mainList = await db.collection(BUILD_COLLECTION).find({
+            
+            'price' : {
+                '$gte': parseInt(req.query.priceLower) ,
+                '$lte': parseInt(req.query.priceUpper) 
+            },
             'cpu_brand': {
-                '$regex': req.params.brand_name,
+                '$regex': req.query.brand_name,
                 '$options':'i'
             }
-        }).project({
-            'name': 1,
-            'image': 1,
-            'votes' : 1,
-        }).toArray();
-        res.send({
-             "brand-build": mainList
-        })
+
+        }).toArray()
+      
+        res.send(mainList)
     })
+
     // axios.get("http://ldkndkfn"+ "/main/brand/ " +this.state.searchBrand)
-    //====================BUILD: FILTER (PRICE) MAIN LOAD==========================//
-    //READ
-    app.get('/:price1/:price2/build', async function(req,res){        
-        const db = MongoUtil.getDB();
-        let mainList = await db.collection(BUILD_COLLECTION).find({
-            'price' : {
-                '$gte': parseInt(req.params.price1),
-                '$lte': parseInt(req.params.price2)
-            }
-        }).project({
-            'name': 1,
-            'image': 1,
-            'votes' : 1,
-        }).toArray();
-        res.send({
-             "price-build": mainList
-        })
-    })
+
     //====================BUILD: INDIVIDUAL PAGE LOAD==========================//
     //READ
     app.get('/:id/individualbuild', async function(req,res){
@@ -87,6 +72,18 @@ async function main(){
         //get main details of listing
         let mainList = await db.collection(BUILD_COLLECTION).find({
             '_id': ObjectId(req.params.id)
+        }).project({
+            '_id' : 1,
+            'name' : 1,
+            'build_ease' : 1,
+            'image' : 1,
+            'price' : 1,
+            'description' : 1,
+            'datetime' : 1,
+            'votes' : 1,
+            'cpu_brand' : 1,
+            'parts' : 1,
+
         }).toArray();
         //get comments of listing
         let commentList = await db.collection(COMMENTS_COLLECTION).find({
@@ -179,27 +176,109 @@ async function main(){
             console.log(e);
         }
     })
-    //====================BUILD: INDIVIDUAL PAGE CHANGE==========================//
-    //READ
-    app.get('/:id/:email/individualbuild', async function(req,res){
-        
-            const db = MongoUtil.getDB();
-            let mainList = await db.collection(BUILD_COLLECTION).find({
-                '_id' : ObjectId(req.params.id)
+    //====================BUILD: INDIVIDUAL PAGE CHANGE(EMAIL VERIFICATION)==========================//
+    // run the route upon user pressing edit button
+    // if the user as entered correct email via query,
+    // confirmation will be send back to client as true
+    // else false
+    app.get('/:id/individualbuild/email', async function(req,res){
+        const db = MongoUtil.getDB();
+        //get main details of listing
+        let mainList = await db.collection(BUILD_COLLECTION).find({
+            '_id': ObjectId(req.params.id),
+        }).project({
+            'email' : 1
+        }).toArray();
+        if(mainList[0].email === req.query.email){
+            res.send({
+                'email_check': true
+            })
+        }
+        else{
+            res.send({
+                'email_check': false
+            })
+        }
+    })
+    //====================BUILD: INDIVIDUAL PAGE CHANGE(EDIT LISTING)==========================//
+    //UPDATE
+    app.put('/:id/individualbuild/edit', async function (req,res){
+        try{
+            let cpuList = await db.collection(CPU_COLLECTION).find({
+                '_id': ObjectId(req.body.cpu)
             }).project({
-                'email' : 1
+                'brand' : 1,
+                'price' : 1
             }).toArray();
-
-            if(mainList[0].email === req.params.email){
-                res.send({
-                    "message": true,
-                })
+    
+            let gpuList = await db.collection(GPU_COLLECTION).find({
+                '_id': ObjectId(req.body.gpu)
+            }).project({
+                'price' : 1
+            }).toArray();
+    
+            let moboList = await db.collection(MOBO_COLLECTION).find({
+                '_id': ObjectId(req.body.mobo)
+            }).project({
+                'price' : 1
+            }).toArray();
+    
+            let ramList = await db.collection(RAM_COLLECTION).find({
+                '_id': ObjectId(req.body.ram)
+            }).project({
+                'price' : 1
+            }).toArray();
+    
+            let name = req.body.name
+            let build_ease = req.body.build_ease
+            let image = req.body.image
+            let price = parseFloat(cpuList[0].price) + parseFloat(gpuList[0].price) + parseFloat(moboList[0].price) + parseFloat(ramList[0].price)
+            let description = req.body.description
+            let cpu_brand = cpuList[0].brand
+            let parts = {
+                'cpu_id': ObjectId(req.body.cpu),
+                'gpu_id': ObjectId(req.body.gpu),
+                'mobo_id': ObjectId(req.body.mobo),
+                'ram_id': ObjectId(req.body.ram)
             }
-            else{
-                res.send({
-                    "message": false
-                })
-            }
+            await db.collection(BUILD_COLLECTION).insertOne({
+                name,
+                build_ease,
+                image,
+                price,
+                description,
+                datetime,
+                votes,
+                cpu_brand,
+                parts,
+                email
+            });
+            res.status(200);
+            res.json({
+                'message':'The record has been edited successfully'
+            })
+        }
+        catch (e){
+            res.status(500);
+            res.json({
+                'message':"Internal server error. Please contact administrator"
+            })
+            console.log(e);
+        }
+    })
+    //====================BUILD: INDIVIDUAL PAGE CHANGE(DELETE LISTING)==========================//
+    //DESTROY
+    app.delete('/:id/individualbuild/delete', async function (req,res){
+        await MongoUtil.getDB().collection(BUILD_COLLECTION).delete({
+            '_id':ObjectId(req.params.id)
+        })
+        await MongoUtil.getDB().collection(COMMENTS_COLLECTION).delete({
+            'build':ObjectId(req.params.id)
+        })
+        res.status(200);
+        res.json({
+            'message':'The document has been deleted'
+        })
     })
     //====================BUILD: NEW LISTING PARTS LOAD==========================//
     //READ
@@ -272,7 +351,7 @@ async function main(){
             let image = req.body.image
             let price = parseFloat(cpuList[0].price) + parseFloat(gpuList[0].price) + parseFloat(moboList[0].price) + parseFloat(ramList[0].price)
             let description = req.body.description
-            let datetime = new Date().toLocaleString() //3/22/2022, 2:34:36 PM
+            let datetime = new Date() //3/22/2022, 2:34:36 PM
             let votes = 0
             let cpu_brand = cpuList[0].brand
             let email = req.body.email
@@ -347,8 +426,8 @@ async function main(){
     })
 }
 main();
-
+// console.log(process.env.DBNAME)
 //LISTENT
-app.listen(process.env.PORT, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server has started")
 })
